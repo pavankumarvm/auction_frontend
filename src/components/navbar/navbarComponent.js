a869efb0375cf3222c297886ff66c2c232d3ac45
pavankumarvm <pavankumarmaurya1999@gmail.com>
2021-10-14 12:47:58 +0530

    Navbar component

---
+++ src/components/navbar/navbarComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  // @Override method
+  render = () => {
+    return <div>Navbar</div>;
+  };
+}
+
+export default Navbar;
